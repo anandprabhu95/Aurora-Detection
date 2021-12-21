4d68cb4479441238208d2d68be164f909e4b739d
anandprabhu95 <anandprabhu95@outlook.com>
2021-12-21 15:00:11 +0100

    Test

--- aurora_detect.py
+++ aurora_detect.py
@@ -1,15 +1,24 @@
 import cv2
+import numpy as np
 
 url = 'http://192.168.0.5:8080/video'
 cap = cv2.VideoCapture(url)
 
-while(True):
+while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_green = np.array([50, 10, 50])
+    upper_green = np.array([255, 200, 255])
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('frame', result)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        cap.release()
         cv2.destroyAllWindows()
         break
-
 
 
 
